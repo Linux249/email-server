fe889ffd597622b6a9410da81ffdce7ea1ca29f0
julian <julian.libor@gmail.com>
2017-10-04 00:22:15 +0200

    reorder passport + app

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,14 @@
+const passport = require('passport')
+
+module.exports = app => {
+    // login via google
+    app.get(
+        '/auth/google',
+        passport.authenticate('google', {
+            scope: ['profile', 'email']
+        })
+    )
+
+    // google api callback
+    app.get('/auth/google/callback', passport.authenticate('google'))
+}
