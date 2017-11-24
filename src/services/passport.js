61d4b91ba149aed17bcf5bf4dbd030d1fbbb8ce9
julian libor <Linux249@users.noreply.github.com>
2017-11-24 17:47:33 +0100

    add User

--- src/services/passport.js
+++ src/services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport')
 const GoogleStrategy= require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
 const keys = require('../config/keys')
+
+const User = mongoose.model('user')
 
 passport.use(
     new GoogleStrategy(
@@ -10,6 +13,7 @@ passport.use(
             callbackURL: '/auth/google/callback'
         },
         (accessToken, refreshToken, profile, done) => {
+            new User({googleID: profile.id}).save()
             console.log({accessToken})
             console.log({refreshToken})
             console.log({profile})
